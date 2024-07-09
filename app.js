097bfd6a2a32a6c6b32f55ba461a0dc7af6c29b5
dotanGit <frenkeldotan1998@gmail.com>
2024-07-09 18:33:44 +0300

    Initial commit
    downloading relevant libraries,
    connection to mongoDB dummy database.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const env = process.env.NODE_ENV || 'local';
+require('custom-env').env(env, './config');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json());
+
+app.listen(process.env.PORT);

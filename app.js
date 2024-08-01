4fbc0874772267616b7d090c8be9f5ca316c5435
dotanGit <frenkeldotan1998@gmail.com>
2024-08-01 12:26:13 +0300

    Add About page
    added route and design

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 const home = require('./routes/home');
 const shop = require('./routes/shop');
 const blog = require('./routes/blog');
+const about = require('./routes/about');
 const products = require('./routes/products');
 const category = require('./routes/category');
 
@@ -38,6 +39,7 @@ app.use(methodOverride('_method'));
 app.use('/', home);
 app.use('/shop', shop);
 app.use('/blog', blog);
+app.use('/about', about);
 app.use('/products', products);
 app.use('/categories', category);
 

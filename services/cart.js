f353ff713c4f4cd144ac7bccaed7614ddf3de683
dotanGit <frenkeldotan1998@gmail.com>
2024-08-06 00:35:34 +0300

    cart.ejs connected to db

--- services/cart.js
+++ services/cart.js
@@ -1,14 +1,26 @@
 const User = require('../models/User');
+const Product = require('../models/products');
 
 async function getCart(userId) {
   const user = await User.findById(userId);
-  return user.cart;
+  const cartItems = await Promise.all(user.cart.map(async item => {
+    const product = await Product.findById(item.productId);
+    const productPrice = parseFloat(product.price); // Convert price to a number
+    return {
+      productId: item.productId,
+      quantity: item.quantity,
+      productName: product.name,
+      productPrice: productPrice,
+      description: product.description,
+      totalPrice: item.quantity * productPrice
+    };
+  }));
+  return cartItems;
 }
 
 async function addToCart(userId, productId, quantity) {
   const user = await User.findById(userId);
-
-  const quantityNumber = parseInt(quantity, 10); // Convert quantity to a number
+  const quantityNumber = parseInt(quantity, 10);
 
   const cartItem = user.cart.find(item => item.productId === productId);
   if (cartItem) {

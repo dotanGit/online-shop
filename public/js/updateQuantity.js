a9c1accd9017168a92276d7301fb48795e6f2fa8
dotanGit <frenkeldotan1998@gmail.com>
2024-08-11 05:19:16 +0300

    quantitiy min value

--- public/js/updateQuantity.js
+++ public/js/updateQuantity.js
@@ -1,16 +1,22 @@
 document.querySelectorAll('input[type="number"]').forEach(input => {
     input.addEventListener('change', async function() {
-        const productId = this.closest('tr').dataset.productId; // Assuming each row has data-product-id
-        const quantity = this.value;
 
-        // Send the updated quantity to the server
-        await fetch('/cart/update-quantity', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ productId, quantity }),
-        });
+        if (this.value < 0) {
+            this.value = 1;
+        }
+        else {
+            const productId = this.closest('tr').dataset.productId; // Assuming each row has data-product-id
+            const quantity = this.value;
+
+            // Send the updated quantity to the server
+            await fetch('/cart/update-quantity', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ productId, quantity }),
+            }); 
+        }
 
         // Optionally, refresh the page to see the updated cart totals
         location.reload();

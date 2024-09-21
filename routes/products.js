dc5d8d03aae87f0303d2822d4bd20565ca54ae90
dotanGit <frenkeldotan1998@gmail.com>
2024-09-21 01:39:20 -0400

    Check point

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const productsContorller = require('../controllers/products')
 
-router.get('/',productsContorller.getProducts);  // This will be /products/
-router.post('/',productsContorller.addProduct);  // This will be /products/
+router.get('/',productsContorller.getProducts);  
+router.post('/',productsContorller.addProduct);  
 router.delete('/:id',productsContorller.deleteProduct);
 router.put('/:id',productsContorller.updateProduct); 
 router.get('/product/:id',productsContorller.getProductById);
